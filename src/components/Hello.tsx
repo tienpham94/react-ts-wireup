9f219480070ad45f27b6e114a51daa203f63fd5c
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-10 23:14:53 +0300

    Finish

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -5,9 +5,11 @@ import "./Hello.css";
 export interface Props {
   name: string;
   enthusiasmLevel?: number;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: Props) {
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
   if (enthusiasmLevel <= 0) {
     throw new Error("You could be a little more enthusiastic. :D");
   }
@@ -16,6 +18,10 @@ function Hello({ name, enthusiasmLevel = 1 }: Props) {
     <div className="hello">
       <div className="greeting">
         Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+      <div>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={onIncrement}>+</button>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import Hello from "./components/Hello";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import { enthusiasm } from "./reducers/index";
+// import { StoreState } from "./types/index";
+import Hello from "./containers/Hello";
+
+const store = createStore(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: "TypeScript"
+});
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById("root") as HTMLElement
 );
